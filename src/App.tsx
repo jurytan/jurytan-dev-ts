65cfe4cea61c5cf8d08d89a9cff99c4be3a0b9a1
jnguyen7410 <jnguyen7410@gmail.com>
2020-08-13 03:44:39 -0700

    Added theme and simple components for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from '@rmwc/button';
+import {
+  Card,
+  CardPrimaryAction,
+  CardMedia,
+  CardActions,
+  CardActionButton,
+  CardActionButtons,
+  CardActionIcon,
+  CardActionIcons,
+} from '@rmwc/card';
+import { Typography } from '@rmwc/typography';
+// import logo from './logo.svg';
 import './App.css';
+import 'rmwc/dist/styles';
+import '@material/button/dist/mdc.button.css';
+import '@rmwc/card/styles';
+import '@rmwc/theme/styles';
+// import  { ThemeProvider } from '@rmwc/theme/dist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App">
+        <header className="App-header">
+        <Typography
+          use="headline1"
+          tag="h1"
+          theme="textSecondaryOnBackground"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          jurytan
+        </Typography>
+        <Typography
+          use="subtitle1"
+          tag="h3"
+          theme="textSecondaryOnBackground"
+          style={{ 
+            marginTop: '-3rem'
+          }}
+        >
+          Software Engineer
+        </Typography>
+        <Button label="Hello Jurytan" theme="primary" />
+        <Card style={{ width: '80%' }}>
+            <CardPrimaryAction>
+              <CardMedia
+                sixteenByNine
+                style={{
+                  backgroundImage: 'url(images/backgrounds/mb-bg-fb-16.png)'
+                }}
+              />
+              <div style={{ padding: '0 1rem 1rem 1rem' }}>
+                <Typography use="headline6" tag="h2">
+                  Our Changing Planet
+                </Typography>
+                <Typography
+                  use="subtitle2"
+                  tag="h3"
+                  theme="textSecondaryOnBackground"
+                  style={{ marginTop: '-1rem' }}
+                >
+                  by Kurt Wagner
+                </Typography>
+                <Typography
+                  use="body1"
+                  tag="div"
+                  theme="textSecondaryOnBackground"
+                >
+                  Visit ten places on our planet that are undergoing the biggest
+                  changes today.
+                </Typography>
+              </div>
+            </CardPrimaryAction>
+            <CardActions>
+              <CardActionButtons>
+                <CardActionButton>Read</CardActionButton>
+                <CardActionButton>Bookmark</CardActionButton>
+              </CardActionButtons>
+              <CardActionIcons>
+                <CardActionIcon onIcon="favorite" icon="favorite_border" />
+                <CardActionIcon icon="share" />
+                <CardActionIcon icon="more_vert" />
+              </CardActionIcons>
+            </CardActions>
+          </Card>
+        </header>
+      </div>
   );
+  
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import  { ThemeProvider } from '@rmwc/theme/dist';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider options={{
+      primary: '#24aee9',
+      secondary: '#e539ff',
+      error: '#b00020',
+      background: '#212121',
+      surface: '#37474F',
+      onPrimary: 'rgba(255,255,255,.87)',
+      onSecondary: 'rgba(0,0,0,0.87)',
+      onSurface: 'rgba(255,255,255,.87)',
+      onError: '#fff',
+      textPrimaryOnBackground: 'rgba(255, 255, 255, 1)',
+      textSecondaryOnBackground: 'rgba(255, 255, 255, 0.7)',
+      textHintOnBackground: 'rgba(255, 255, 255, 0.5)',
+      textDisabledOnBackground: 'rgba(255, 255, 255, 0.5)',
+      textIconOnBackground: 'rgba(255, 255, 255, 0.5)',
+      textPrimaryOnLight: 'rgba(0, 0, 0, 0.87)',
+      textSecondaryOnLight: 'rgba(0, 0, 0, 0.54)',
+      textHintOnLight: 'rgba(0, 0, 0, 0.38)',
+      textDisabledOnLight: 'rgba(0, 0, 0, 0.38)',
+      textIconOnLight: 'rgba(0, 0, 0, 0.38)',
+      textPrimaryOnDark: 'white',
+      textSecondaryOnDark: 'rgba(255, 255, 255, 0.7)',
+      textHintOnDark: 'rgba(255, 255, 255, 0.5)',
+      textDisabledOnDark: 'rgba(255, 255, 255, 0.5)',
+      textIconOnDark: 'rgba(255, 255, 255, 0.5)'
+    }}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
