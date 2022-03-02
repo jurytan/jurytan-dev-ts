1a1c3d9be7818191b9c2b0077f2f576220c6f9b7
jnguyen7410 <jnguyen7410@gmail.com>
2022-03-02 15:59:13 -0800

    Removing placeholder card

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,20 +1,6 @@
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
-import { Typography } from "@mui/material";
 import { JobHistory } from "./JobHistory";
 import { Skills } from "./Skills";
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
 
 export const Main = () => {
   return (
@@ -27,29 +13,6 @@ export const Main = () => {
       >
         Hello Jurytan
       </Button>
-      <Card sx={{ minWidth: 275, borderRadius: 5 }}>
-        <CardContent sx={{ padding: 4 }}>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="div">
-            be{bull}nev{bull}o{bull}lent
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" sx={{ pb: 1, pl: 2 }}>
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
       <br />
       <JobHistory />
       <br />
