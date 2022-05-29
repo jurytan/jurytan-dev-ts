a044db206ecdfc34244707c52fc833c3c665adf9
jurytan <jnguyen7410@gmail.com>
2022-05-29 03:00:03 -0400

    Adding new logic to deal with hand waving without relying on snackbar

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,66 +1,37 @@
-import * as React from 'react';
 import Button from "@mui/material/Button";
-import Snackbar from '@mui/material/Snackbar';
-import SnackbarContent from '@mui/material/SnackbarContent';
-import Slide, { SlideProps } from '@mui/material/Slide';
-
+import PropTypes from 'prop-types';
 import { JobHistory } from "./JobHistory";
 import { Skills } from "./Skills";
 
-type TransitionProps = Omit<SlideProps, 'direction'>;
-
-function TransitionUp(props : TransitionProps) {
-  return <Slide {...props} direction="up" />;
+interface MainProps {
+  handleChange: Function,
 }
 
-export const Main = () => {
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpen(false);
-  };
+export const Main = (props : MainProps) => {
+  function handleChange() {
+    props.handleChange();
+  }
 
   return (
     <div className="main">
-      <Button
-        variant="text"
-        sx={{
-          mb: "2rem",
-        }}
-        onClick={handleClick}
-      >
-        WAVE
-      </Button>
-      <Snackbar
-        open={open}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        message="👋"
-        TransitionComponent={TransitionUp}
-      >
-        <SnackbarContent 
-            message="👋"
-            elevation={0}
-            sx={{
-              background: 'none',
-              fontSize: '5rem',
-            }}
-        />
-      </Snackbar>
-      <br />
-      <JobHistory />
-      <br />
-      <Skills />
-      <br />
+        <Button
+          variant="text"
+          sx={{
+            mb: "2rem",
+          }}
+          onClick={handleChange}
+        >
+          WAVE
+        </Button>
+        <br />
+        <JobHistory />
+        <br />
+        <Skills />
+        <br />
     </div>
   );
+};
+
+Main.propType = {
+  handleChange: PropTypes.func.isRequired,
 };
--- src/containers/Portfolio.tsx
+++ src/containers/Portfolio.tsx
@@ -1,8 +1,12 @@
+import * as React from 'react';
 import Container from "@mui/material/Container";
 import { Header } from "./Header";
 import { Main } from "./Main";
 import { BackgroundFooter } from "../components/BackgroundFooter";
 import { Intro } from "./Intro";
+import Box from '@mui/material/Box';
+import Slide from '@mui/material/Slide';
+import handwave from '../images/hand-wave.png';
 
 // Adding imports for fontsource
 import "@fontsource/roboto/300.css";
@@ -12,15 +16,33 @@ import "@fontsource/roboto/700.css";
 import { Footer } from "./Footer";
 
 export const Portfolio = () => {
+
+  const [show, setShow] = React.useState(false);
+  const containerRef = React.useRef(null);
+
+  const handleChange = () => {
+      setShow((prev : boolean) => !prev);
+      setTimeout(function () {
+        setShow(false);
+    }, 5000)
+  };
+
   return (
     <div className="Portfolio">
-      <Container maxWidth="md">
-        <Header />
-        <Intro />
-        <Main />
-        <Footer />
-        <BackgroundFooter />
-      </Container>
+      <Box ref={containerRef}>
+        <Container maxWidth="md">
+          <Header />
+          <Intro />
+          <Main handleChange={handleChange} />
+          <Footer />
+          <BackgroundFooter />
+        </Container>
+        <Box className='handwave' sx={{ width: 10 }}>
+          <Slide direction="up" in={show} container={containerRef.current}>
+            <img src={handwave} width={50}/>
+          </Slide>
+        </Box>
+      </Box>
     </div>
   );
 };
